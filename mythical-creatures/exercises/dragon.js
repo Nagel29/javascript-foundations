283ed1f363e664ebcf913bf8ce7632d602d41c3a
Ryan Nagel <RNagel29@gmail.com>
2022-08-12 14:32:13 -0600

    Add 2 completed creatures

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor (name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    hungryCount++;
+    if (hungryCount >= 3) {
+      this.hungry = false;
+    } else {
+      this.hungry = true;
+    }
+    }
+}
+let hungryCount = 0
 
+
+module.exports = Dragon;
