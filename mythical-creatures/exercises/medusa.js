8ec152e2a76410fb321280661d3b70470c7d52ac
Ryan Nagel <RNagel29@gmail.com>
2022-08-15 13:11:57 -0600

    Completed medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,28 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    var newStatue = new Statue(victim.name);
+      if (this.statues.length < 3) {
+        this.statues.push(newStatue);
+        console.log(this.statues);
+        console.log(this.statues.length);
+      } else {
+        var releasedVictim = new Person(this.statues[0].name);
+        releasedVictim.mood = 'relieved';
+        this.statues.shift();
+        this.statues.push(newStatue);
+        console.log(this.statues);
+        console.log(this.statues.length);
+        return releasedVictim;
+    }
+  }
+}
+
+
+module.exports = Medusa;
