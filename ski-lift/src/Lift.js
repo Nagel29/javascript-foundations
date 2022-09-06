ec6404ff9c9a915e668b74d52ebe92b974610f53
Ryan Nagel <RNagel29@gmail.com>
2022-09-06 08:55:25 -0600

    Add completed lift and skier

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,34 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+
+  admitSkier(name, hasLiftTicket) {
+    if (!hasLiftTicket) {
+      return `Sorry, ${name}. You need a lift ticket!`;
+    }
+    if (this.skiers.length < this.limit) {
+        var skier = new Skier(name, hasLiftTicket);
+        this.skiers.push(skier);
+    }
+      return `Sorry, ${name}. Please wait for the next lift!`;
+  }
+
+  startLift() {
+    if (this.skiers.length === this.limit) {
+      this.safetyBar = 'down';
+    }
+    var openSeats = this.limit - this.skiers.length;
+    if (openSeats > 1) {
+      return `We still need ${openSeats} more skiers!`;
+    }
+      return `We still need ${openSeats} more skier!`;
+  }
 }
 
 module.exports = Lift;
--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,23 @@
 class Skier {
+  constructor(name, hasLiftTicket) {
+    this.name = name;
+    this.hasLiftTicket = hasLiftTicket;
+    this.skillLevel = 1;
+    this.nextSlope = 'green circle';
+  }
 
+  takeLesson() {
+    this.skillLevel++;
+  }
+
+  pickSlope() {
+    if (this.skillLevel >= 5) {
+      return this.nextSlope = 'black diamond';
+    }
+    if (this.skillLevel >= 3) {
+      return this.nextSlope = 'blue square';
+    }
+  }
 }
 
 module.exports = Skier;
