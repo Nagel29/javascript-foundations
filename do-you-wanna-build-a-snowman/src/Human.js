37afc1857817871a3db1cfaae60de0d0ac9dff0f
Ryan Nagel <RNagel29@gmail.com>
2022-09-13 08:52:36 -0600

    Add completed snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,6 +1,27 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
+  }
+  gatherMaterials(thing, value) {
+    this.materials[thing] += value;
+  }
+  buildASnowman() {
+    var snowman = new Snowman(this.materials);
+    this.snowman = snowman;
+    return snowman;
+  }
+  placeMagicHat() {
+    return this.snowman.magicHat? 'Woah, this snowman is coming to life!' : 'I guess I didn\'t build it correctly.';
+  }
 
 }
 
