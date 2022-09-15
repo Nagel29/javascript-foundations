ec2aafca6d927fefb06d8a3921b63d35075f946c
Ryan Nagel <RNagel29@gmail.com>
2022-09-15 08:29:35 -0600

    Add completed lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
-
+  constructor(ownerDetails) {
+    this.owner = ownerDetails.owner;
+    this.material = ownerDetails.madeOf;
+    this.shape = ownerDetails.shape;
+    this.color = ownerDetails.color;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks() {
+    var healthySnacks = [];
+    // console.log(this.snacks[1]);
+    // console.log(this.snacks[1].checkForHealthy);
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
