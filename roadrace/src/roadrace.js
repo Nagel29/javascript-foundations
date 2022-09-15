f7935bb8f9df473b29811f8e569ad6ba62966a65
Ryan Nagel <RNagel29@gmail.com>
2022-09-15 08:58:14 -0600

    Add completed roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,22 @@
 class Roadrace {
-
+  constructor(raceDetails) {
+    this.name = raceDetails.title;
+    this.location = raceDetails.city;
+    this.participants = [];
+  }
+  setDistance(miles) {
+    this.distance = miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces.push(this.name);
+      this.participants[i].runSomeMiles(this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
